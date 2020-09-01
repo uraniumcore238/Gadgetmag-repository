f245049af83fbb446b2fa7cbd2510d684f08e2bc
sergei <uraniumcore238@gmail.com>
2020-09-01 09:52:58 +0500

    commit from original gadgetmag

--- Tests/test_checking.py
+++ Tests/test_checking.py
@@ -7,20 +7,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 class TestOne:
 
-    print("zzzzzzzzzzzzzzzzz")
+    print("2222222222222222222222222222")
 
 
     def test_e2e(self):
         driver = webdriver.Chrome(executable_path="D:\\Programming\\Gadgetmag\\Sourse\\chromedriver.exe")
         driver.get("https://www.gadgetmag.su/")
         driver.maximize_window()
-        print("================================")
+        print("22222222222222222222222222222222222222222222222")
 
         # MAIN_BANNER_LOCATOR = (By.XPATH, "//div[@id='page']/section/div[@class='main-slider-banner lapping']")
-        main_banner = WebDriverWait(driver, 20).until(
+        main_banner = WebDriverWait(driver, 22).until(
             expected_conditions.visibility_of_element_located((By.XPATH, "//div[@id='page']/section/div[@class='main-slider-banner lapping']")))
 
-        print("***********************************")
-        time.sleep(10)
+        print("2222222222222222222222222222222222222222")
+        time.sleep(22)
         driver.close()
         driver.quit()
